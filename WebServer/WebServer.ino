ab1b5326036a0da375e6e35002dcb6a3be12e89f
xspager <xspager@gmail.com>
2014-10-06 01:59:55 -0300

    Adding some old stuff

---
+++ WebServer/WebServer.ino
@@ -0,0 +1,172 @@
+/*
+  Web Server
+ 
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield. 
+ 
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+ 
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ 
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include "index.h"
+
+#define BUF_LEN 100
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192,168,0, 100);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+ // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+  Serial.println(GetTemp(),1);
+}
+
+void loop() {
+  char clientline[BUF_LEN];
+  int return_json = 0;
+
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    int index = 0;
+    
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+                
+        if (c != '\n' && c != '\r') {
+          clientline[index] = c;
+          index++;
+          // are we too big for the buffer? start tossing out data
+          if (index >= BUF_LEN) 
+            index = BUF_LEN -1;
+ 
+          // continue to read more data!
+          continue;
+        }
+ 
+        // got a \n or \r new line, which means the string is done
+        clientline[index] = 0;
+        
+        // Print it out for debugging
+        Serial.println(clientline);
+        Serial.print("\n");
+        
+        if (strstr(clientline, "GET /data.json ") != 0) {
+            return_json = 1;
+        }
+        
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+
+            if(return_json){
+              // send json
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: application/json");
+              client.println();
+              
+              // output the value of each analog input pin
+              client.print("{");
+              for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+                int sensorReading = analogRead(analogChannel);
+                client.print("\"A");
+                client.print(analogChannel);
+                client.print("\": ");
+                client.print(sensorReading);
+                if(analogChannel < 5)
+                    client.println(",");       
+              }
+              client.print("}");
+            } else {
+              // send a standard http response header
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/html");
+              client.println("Connnection: close");
+              client.println();
+              client.println("<!DOCTYPE HTML>");
+              client.println(html);
+            }       
+            break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+
+double GetTemp(void)
+{
+  unsigned int wADC;
+  double t;
+
+  // The internal temperature has to be used
+  // with the internal reference of 1.1V.
+  // Channel 8 can not be selected with
+  // the analogRead function yet.
+
+  // Set the internal reference and mux.
+  ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+  ADCSRA |= _BV(ADEN);  // enable the ADC
+
+  delay(20);            // wait for voltages to become stable.
+
+  ADCSRA |= _BV(ADSC);  // Start the ADC
+
+  // Detect end-of-conversion
+  while (bit_is_set(ADCSRA,ADSC));
+
+  // Reading register "ADCW" takes care of how to read ADCL and ADCH.
+  wADC = ADCW;
+
+  // The offset of 324.31 could be wrong. It is just an indication.
+  t = (wADC - 324.31 ) / 1.22;
+
+  // The returned temperature is in degrees Celcius.
+  return (t);
+}
+
